7b1e9ba5511e986db918f20b3879d7f58570411c
srdjanpetkovski <srdjanpetkovski@gmail.com>
2022-03-04 09:50:48 +0100

    impressions and benefits css

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font Awesome  -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Youth Hub</title>
+</head>
+<body>
+    <!-- logo -->
+    <a class="logo" href="index.php"><img src="img/YH-Beli_bezteksta.png" alt=""></a>
+
+    <!-- menu icon -->
+    <div class="menu-icon">
+        <i class="fas fa-bars"></i>
+    </div>
+    <!-- close icon -->
+    <div class="close-icon">
+        <i class="fas fa-times"></i>
+    </div>
+
+    <!-- header -->
+    <header>
+        <img src="img/YH-Beli.png" alt="">
+        <h1><span>BUDI DEO</span> NAJVEĆE OMLADINSKE VOLONTERSKE MREŽE <span>U NOVOM SADU</span></h1>
+    </header>
+
+    <!-- intro -->
+    <section class="intro container">
+        <iframe width="100%" height="500" src="https://www.youtube.com/embed/UXSAXfKAGh0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p><span>Zdravo,</span> Ako imaš između 15 i 30 godina, a ne znaš na koji način da iskoristiš svoje slobodno vreme, nalaziš se na pravom mestu. <span>Moje ime je Milan i osnivač sam udruženja pod nazivom Omladinska mreža Novi Sad (Youth Hub Novi Sad). Ono je nastalo iz moje zamisli da se mladima obezbedi prostor za realizaciju svojih aktivnosti i projekata, ali i da se zadovolje njihove potrebe za razvojem i samoaktualizacijom.</span> Takođe, Omladinska mreža Novi Sad okuplja nekoliko omladinskih udruženja koja se bave omladinskom politikom. Moj cilj je da se stvori okruženje koje će omogućiti mladima da se povežu sa ovim udruženjima i da se kroz taj kontakt ostvare interesi omladine u Novom Sadu.</p>
+    </section>
+
+    <!-- accents -->
+    <section class="accents">
+        <article class="container">
+            <p>Stalno slušam kako mladi ne preduzimaju ništa kako bi poboljšali svoj položaj u zajednici u kojoj žive i kako su ravnodušni prema svojoj budućnosti. Mnogi razmišljaju i o odlasku iz države.<br><br> Zbog toga sam odlučio da uradim ono što je u mojoj moći i kreiram uslove za uključivanje omladine Novog Sada u društvene tokove i korišćenje njihovog punog potencijala.</p>
+        </article>
+    </section>
+
+    <!-- program -->
+    <section class="program container">
+        <p>OMLADINSKA MREŽA NOVI SAD NUDI TI SVOJ COWORKING PROSTOR ZA REALIZACIJU AKTIVNOSTI I PROJEKATA, BILO DA SU U PITANJU INDIVIDUALNE ILI GRUPNE AKTIVNOSTI.<br><br> UKOLIKO SE PRIJAVIŠ NA NAŠ VOLONTERSKI PROGRAM U OKVIRU ODELJKA VOLONTERSKA MREŽA, MOŽEŠ DA BUDEŠ DEO PROJEKTA NEKE OD ORGANIZACIJA OKUPLJENIH OKO OMLADINSKE MREŽE I TIME STEKNEŠ NEPROCENJIVO ISKUSTVO KOJE TI MOŽE ZNAČITI U PROFESIONALNOM I PRIVATNOM ŽIVOTU.</p>
+    </section>
+
+    <!-- impressions -->
+    <section class="impressions container">
+        <h2>Utisci korisnika prostora</h2>
+        <article>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Milan Krstic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Milica Petrovic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Jelena Bobic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Nikola Savic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+        </article>
+    </section>
+
+    <!-- benefits -->
+    <section class="benefits container">
+        <article>
+            <div>
+                <h3>Benefiti</h3>
+                <ul>
+                    <li>REDOVNI SASTANCI SA PROJEKTNIM MENADŽEROM I DRUGIM VOLONTERIMA</li>
+                    <li>OBUKA PRE POČETKA VOLONTERSKOG RADA</li>
+                    <li>UČEŠĆE NA TIM BILDINZIMA ORGANIZACIJE</li>
+                    <li>PRAVO NA BESPLATNO UČEŠĆE NA OBUKAMA U OKVIRU ORGANIZACIJE OMLADINSKA MREŽA NOVI SAD</li>
+                    <li>POTVRDA O VOLONTERSKOM ANGAŽMANU</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Dodatni benefiti</h3>
+                <ul>
+                    <li>NOVO ISKUSTVO</li>
+                    <li>NOVA ZNANJA</li>
+                    <li>NOVA POZNANSTVA</li>
+                </ul>
+            </div>
+        </article>
+    </section>
+
+    <!-- impressions -->
+    <section class="impressions container">
+        <h2>Utisci volontera</h2>
+        <article>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Nikola Petrovic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Danijel Markovic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Milos Nikolic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+            <div>
+                <img src="img/people.jpg" alt="">
+                <h3>Ivana Velickovic</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero.</p>
+            </div>
+        </article>
+    </section>
+    
+</body>
+</html>
---
+++ partials/navbar.php
@@ -0,0 +1,5 @@
+<?php include "partials/navbar.php" ?>
+
+<h1 style="font-size:50px; text-align:center;">footer</h1>
+
+<?php include "partials/footer.php" ?>
